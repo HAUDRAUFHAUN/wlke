358f2fcacf3445d378069b3f39032a397c90f7c3
HAUDRAUFHAUN <47633893+HAUDRAUFHAUN@users.noreply.github.com>
2020-05-19 13:53:40 +0200

    jumbo test

--- wlke/index/views.py
+++ wlke/index/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from .models import Jumbo
+
 # Create your views here.
+
+
 def index(request):
-    return render(request, 'index/index.html')
+    jumbo_data = Jumbo.objects.get(pk=1)
+    frontend_data = {'jumbo_data': jumbo_data, }
+    return render(request, 'index/index.html', frontend_data)
