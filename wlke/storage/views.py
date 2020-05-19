a2b106c85aff3b5f636d7e4b0132a3fde4af3462
HAUDRAUFHAUN <47633893+HAUDRAUFHAUN@users.noreply.github.com>
2020-05-19 16:47:48 +0200

    Update views.py

--- wlke/storage/views.py
+++ wlke/storage/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from .models import Datei
+
 # Create your views here.
+
+
 def index(request):
-    return render(request, '')
+    return render(request, 'storage/storage_index.html')
