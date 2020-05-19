10642ffb08afed3927dc62e07523c45c348a8a8a
HAUDRAUFHAUN <47633893+HAUDRAUFHAUN@users.noreply.github.com>
2020-05-19 13:48:14 +0200

    Admin Test

--- wlke/index/models.py
+++ wlke/index/models.py
@@ -7,3 +7,6 @@ class Jumbo(models.Model):
     titel = models.CharField(max_length=100)
     untertitel = models.CharField(max_length=250)
     inhalt = models.TextField()
+
+    def __str__(self):
+        return '{}'.format(self.titel)
