a7b54bbcb3f854c1868236c6b2a1f1f5a4dad0b9
HAUDRAUFHAUN <47633893+HAUDRAUFHAUN@users.noreply.github.com>
2020-05-19 13:32:25 +0200

    Update models.py

--- wlke/index/models.py
+++ wlke/index/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Jumbo(models.Model):
+    titel = models.CharField(max_length=100)
+    untertitel = models.CharField(max_length=250)
+    inhalt = models.TextField()
