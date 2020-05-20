1d17a9a75aaa72fc55eb9ec85edc4a66da38f6cd
HAUDRAUFHAUN <47633893+HAUDRAUFHAUN@users.noreply.github.com>
2020-05-20 17:09:04 +0200

    Update forms.py
    Build Registration form

--- wlke/accounts/forms.py
+++ wlke/accounts/forms.py
@@ -1,5 +1,11 @@
 from django import forms
-from django.contrib.auth import authenticate
+from django.contrib.auth import (
+    authenticate,
+    get_user_model
+)
+
+Benutzer = get_user_model()
+
 
 class UserLoginForm(forms.Form):
     benutzername = forms.CharField()
@@ -18,6 +24,42 @@ class UserLoginForm(forms.Form):
             if not benutzer.is_active:
                 raise forms.ValidationError('Dieser Benutzer ist nicht aktiv!')
         return super(UserLoginForm, self).clean(*args, **kwargs)
-        
 
-                
+
+class UserRegisterForm(forms.ModelForm):
+    email = forms.EmailField(label='E-Mail Adresse')
+    email2 = forms.EmailField(label='E-Mail Bestätigung')
+    passwort = forms.CharField(widget=forms.PasswordInput)
+    passwort2 = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = Benutzer
+        fields = [
+            'benutzername',
+            'email',
+            'email2',
+            'passwort',
+            'passwort2',
+        ]
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email2 = self.cleaned_data.get('email2')
+        passwort = self.cleaned_data.get('passwort')
+        passwort2 = self.cleaned_data.get('passwort2')
+
+        if email != email2:
+            raise forms.ValidationError('E-Mail Adressen stimmen nicht überein!')
+
+        email_qs = Benutzer.objects.filter(email=email)
+
+        if email_qs.exists():
+            raise forms.ValidationError('DieseE-Mail wurde bereits registriert!')
+
+        return email
+
+        if passwort != passwort2:
+            raise forms.ValidationError('Passwörter müssen übereinstimmen!')
+
+
+        
