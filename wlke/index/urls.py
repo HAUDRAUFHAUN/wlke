4b4b3243a8ff82a817e110b9e3aac642be40421f
HAUDRAUFHAUN <47633893+HAUDRAUFHAUN@users.noreply.github.com>
2020-05-20 12:31:42 +0200

    Create Django Detail view

--- wlke/index/urls.py
+++ wlke/index/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('jumbo/<int:jumbo_id>/', views.jumbo_detail, name="jumbodetail"),
 ]
--- wlke/index/views.py
+++ wlke/index/views.py
@@ -9,3 +9,9 @@ def index(request):
     jumbo_data = Jumbo.objects.get(pk=1)
     frontend_data = {'jumbo_data': jumbo_data, }
     return render(request, 'index/index.html', frontend_data)
+
+
+def jumbo_detail(request, jumbo_id):
+    jumbo_Data = Jumbo.objects.get(pk=jumbo_id)
+    frontend_stuff = {'jumbo_data': jumbo_Data, }
+    return render(request, 'index/jumbo_detail.html', frontend_stuff)
