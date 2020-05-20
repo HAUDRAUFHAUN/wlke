b89c92b0d14ff906272e25617547e5511b50f810
HAUDRAUFHAUN <47633893+HAUDRAUFHAUN@users.noreply.github.com>
2020-05-20 16:55:34 +0200

    Update forms.py
    finish login form

--- wlke/accounts/forms.py
+++ wlke/accounts/forms.py
@@ -11,5 +11,13 @@ class UserLoginForm(forms.Form):
 
         if benutzername and passwort:
             benutzer = authenticate(username=benutzername, password=passwort)
-            if not user:
-                raise forms.ValidationError('Dieser Benutzer existiert nicht')
+            if not benutzer:
+                raise forms.ValidationError('Dieser Benutzer existiert nicht!')
+            if not benutzer.check_password(passwort):
+                raise forms.ValidationError('Falsches Passwort!')
+            if not benutzer.is_active:
+                raise forms.ValidationError('Dieser Benutzer ist nicht aktiv!')
+        return super(UserLoginForm, self).clean(*args, **kwargs)
+        
+
+                
