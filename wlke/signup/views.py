2f2cd5b8d3b646f6c4ea0b8dacb7df90cb84502a
HAUDRAUFHAUN <47633893+HAUDRAUFHAUN@users.noreply.github.com>
2020-05-21 19:13:52 +0200

    add new form to views

--- wlke/signup/views.py
+++ wlke/signup/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render, redirect
 
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from .forms import SignupForm
 
 # Create your views here.
 
 
 def signup(response):
     if response.method == "POST":
-        form = UserCreationForm(response.POST)
+        form = SignupForm(response.POST)
         if form.is_valid():
             form.save()
 
             return redirect('/storage')
     else:
-        form = UserCreationForm
+        form = SignupForm
     return render(response, 'signup/signup.html', {'form': form})
