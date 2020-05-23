2d25ebb24165533ada62642818ae297cb8f5a9f8
HAUDRAUFHAUN <47633893+HAUDRAUFHAUN@users.noreply.github.com>
2020-05-23 19:45:20 +0200

    setup the models and database new

--- wlke/storage/models.py
+++ wlke/storage/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Datei(models.Model):
+    benutzer = models.ForeignKey(User, on_delete=models.CASCADE)
     datei = models.FileField()
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
