fd45edb2541c26afbee58c54ebaa6576db255129
HAUDRAUFHAUN <47633893+HAUDRAUFHAUN@users.noreply.github.com>
2020-05-29 18:21:29 +0200

    get started with file backend an added missing favicons

--- wlke/storage/views.py
+++ wlke/storage/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 from .models import Datei
 
 # Create your views here.
 
 
-def index(request):
-    neue_datei = request.POST.get('datei')
-    print(neue_datei)
+def index(response):
+    
+    if response.method == "POST":
+        neue_datei = response.POST.get('datei')
+        print(neue_datei)
+        t = Datei(datei=neue_datei,)
+        t.save()
+        response.User.add(t)
+        return HttpResponseRedirect("/storage")
+
     dateien = Datei.objects.all().order_by("name")
     frontend_data = {'dateien': dateien, }
-    return render(request, 'storage/storage_index.html', frontend_data)
+    return render(response, 'storage/storage_index.html', frontend_data)
