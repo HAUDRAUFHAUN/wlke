6f1a4b6176b2f7a9a126ff734e9a4507bd424d7e
HAUDRAUFHAUN <dietrich.poensgen@heg-portal.de>
2020-06-16 22:02:30 +0200

    Create Notiz model

--- wlke/notes/models.py
+++ wlke/notes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+
+class Notiz(models.Model):
+    benutzer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="notiz", null=False)
+    titel = models.CharField(max_length=200)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Notizeb'
