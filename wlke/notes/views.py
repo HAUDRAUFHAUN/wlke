2ad18f5e3082aa731a04505f3273a20fd196a393
HAUDRAUFHAUN <dietrich.poensgen@heg-portal.de>
2020-06-22 10:11:39 +0200

    update notes

--- wlke/notes/views.py
+++ wlke/notes/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
+
+    if request.method == 'POST':
+        print(request)
     return render(request, 'notes/notes_index.html')
