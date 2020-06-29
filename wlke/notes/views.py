e8e8818f3bf697cf724e864d929032bcaebd7114
HAUDRAUFHAUN <dietrich.poensgen@heg-portal.de>
2020-06-29 14:26:23 +0200

    fix note edit bug and disable note editing for archived notes

--- wlke/notes/views.py
+++ wlke/notes/views.py
@@ -21,9 +21,8 @@ def index(request):
 
 
 def edit(request, note_id):
-    note = Notiz.objects.all().filter(id=note_id)
-    print(note)
-    return render(request, "notes/notes_edit.html", {"note": note, })
+    frontend_data = {'note_id': note_id}
+    return render(request, "notes/notes_edit.html", frontend_data)
 
 
 def archive(request, note_id):
