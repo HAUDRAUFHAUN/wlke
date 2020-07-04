8ecddd4c9100614a534dc7efe393de99629d59c0
HAUDRAUFHAUN <dietrich.poensgen@heg-portal.de>
2020-07-04 14:44:43 +0200

    add more security to file upload

--- wlke/storage/models.py
+++ wlke/storage/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+import random
+import string
 # Create your models here.
 
 
 class Datei(models.Model):
     benutzer = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="datei", null=True)
+    rand = ''.join(random.choices(
+        string.ascii_uppercase + string.digits, k=20))
     datei = models.FileField(
-        upload_to='data/adjkncvir1495kgsiqiy/', null=True, blank=True,)
+        upload_to='data/' + rand + '/', null=True, blank=True,)
     name = models.CharField(max_length=200)
     uploaded = models.DateTimeField(auto_now_add=True)
 
