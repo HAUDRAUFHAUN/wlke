8b8f0a38eeea2c0d47edd5a69a71fe49b1b62fea
HAUDRAUFHAUN <dietrich.poensgen@heg-portal.de>
2020-07-12 20:37:20 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
 echo "Installation of wlke starts..."
+
+if [ -x "$(command -v docker)" ]; then
+    echo "Update docker"
+    # command
+else
+    echo "Install docker"
+    # command
+fi
+
+docker pull docker.pkg.github.com/haudraufhaun/wlke/wlke
