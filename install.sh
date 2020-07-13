2a9f2a8204c010706d64064cea857ac975a236af
HAUDRAUFHAUN <dietrich.poensgen@heg-portal.de>
2020-07-13 21:37:18 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,9 @@
 echo "Installation of wlke starts..."
 
-if [ -x "$(command -v docker)" ]; then
-    echo "Update docker"
-    # command
+if [ docker -v ] 
+then
+    docker pull docker.pkg.github.com/haudraufhaun/wlke/wlke:0.01.1
+    docker run -dp 8000:8000 --name wlke docker.pkg.github.com/haudraufhaun/wlke/wlke:0.01.1
+fi
 else
     echo "Install docker"
-    # command
-fi
-
-docker pull docker.pkg.github.com/haudraufhaun/wlke/wlke:0.01.1
-
-docker run -dp 8000:8000 --name wlke docker.pkg.github.com/haudraufhaun/wlke/wlke:0.01.1
