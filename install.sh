bfe2c80708e13df25b60e129d274c48009730f35
HAUDRAUFHAUN <47633893+HAUDRAUFHAUN@users.noreply.github.com>
2020-09-13 09:29:41 +0000

    update installation file with docker-compose stup

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@ echo "Installation of wlke starts..."
 
 if [ docker -v ] 
 then
-    docker pull docker.pkg.github.com/haudraufhaun/wlke/wlke:0.01.1
-    docker run -dp 8000:8000 --name wlke docker.pkg.github.com/haudraufhaun/wlke/wlke:0.01.1
+    echo "starting containers..."
+    cd wlke && docker-compose up --build -d
+    sleep 100.0
+    echo "Installation finished"
 fi
 else
     echo "Install docker"
